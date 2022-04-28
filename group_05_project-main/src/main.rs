8613c8369f4d9a793a793ab722a04d5a2aecab1a
Akhil Isanaka <35632416+akpkboss@users.noreply.github.com>
2022-04-28 07:35:24 -0500

    Add files via upload

---
+++ group_05_project-main/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(linked_list_remove)]
+use std::env;
+mod trade;
+mod orderbook;
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = &args[1];
+    if input == "1" || input == "ome" {
+        //run ome
+        println!("hi");
+    }
+}
